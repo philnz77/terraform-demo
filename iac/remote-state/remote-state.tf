53c34e24c7c193349d2e742e25d275b22a58af63
philmora <philnz77@gmail.com>
2024-07-04 01:53:18 +1200

    block public access

--- iac/remote-state/remote-state.tf
+++ iac/remote-state/remote-state.tf
@@ -68,3 +68,12 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption
     }
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
